567bf12d53a49e990c84d5f846c34dfee6d26944
Michelle Tournier <michelletournier@Michelles-MacBook-Air.local>
2020-05-02 23:07:20 -0700

    v1 analysis

--- script.py
+++ script.py
@@ -3,7 +3,10 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.cluster import KMeans
 from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
 
+
+experiment_datetime_version = "2020-05-02-joem_v1"
 
 # loading data from xml
 data = pub.get_dataframe().dropna()
@@ -19,4 +22,19 @@ data["cluster"] = kmeans.labels_
 
 # Adding tsne vector coordinates
 dimensions = TSNE(n_components=2).fit_transform(features.toarray())
+data["coord_x"] =  dimensions[:,0]
+data["coord_y"] =  dimensions[:,1]
 
+# Saving scatterplot to image:
+plt.figure(figsize=(20,10))
+plt.ylim(-40,40)
+groups = data.groupby("cluster")
+for name, group in groups:
+    plt.plot(group["coord_x"], group["coord_y"], marker="o", linestyle="", label=name)
+plt.title("All JOEM Articles, displayed by cluster")
+plt.legend()
+plt.savefig(experiment_datetime_version + ".png")
+plt.show()
+
+# saving to a csv file
+data.to_csv(experiment_datetime_version + ".csv", index=None)
